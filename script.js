88c4fb832f642fa4724ce4da6b55bb454c938d4e
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-08-18 14:21:11 +0600

    added here first and second problem and solving that.

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function reverseString(str) {
     return reversed;
 }
 
+
+
+// problem number 2
+function countVowels(str) {
+    let count = 0;
+    const vowels = "aeiouAEIOU";
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
